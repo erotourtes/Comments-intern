ed925e4aaa6e57b4219d259e7c407279194b9871
erotourtes <erotourtes@gmail.com>
2024-02-01 19:42:41 +0200

    Feat: added posts crud

---
+++ src/posts/dto/create-post.dto.ts
@@ -0,0 +1,19 @@
+import { IsNumber, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @MaxLength(191)
+  title: string;
+
+  @IsString()
+  @MinLength(1)
+  @MaxLength(65_535)
+  content: string;
+
+  @IsString()
+  @MaxLength(191)
+  homepageUrl?: string;
+
+  @IsNumber()
+  authorId: number;
+}
---
+++ src/posts/posts.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class PostsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createPostDto: CreatePostDto) {
+    const post = await this.prisma.post.create({
+      data: createPostDto,
+    });
+
+    return post;
+  }
+
+  async findOne(id: number) {
+    const post = await this.prisma.post.findUnique({
+      where: { id },
+    });
+
+    return post;
+  }
+
+  async update(id: number, updatePostDto: UpdatePostDto) {
+    const post = await this.prisma.post.update({
+      where: { id },
+      data: {
+        ...updatePostDto,
+      },
+    });
+
+    return post;
+  }
+
+  remove(id: number) {
+    this.prisma.post.delete({
+      where: { id },
+    });
+  }
+}
