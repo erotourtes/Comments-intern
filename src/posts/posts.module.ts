2b0aa8f35e00d1521fbfce4c21dea0aec941fd5e
erotourtes <erotourtes@gmail.com>
2024-02-04 11:51:58 +0200

    Fix: nest modules

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   controllers: [PostsController],
-  providers: [PostsService, PrismaService],
+  providers: [PostsService],
+  imports: [PrismaModule],
 })
 export class PostsModule {}
