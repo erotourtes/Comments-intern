e330f8590cb9450fa090d3fd82e62dc83fd53932
erotourtes <erotourtes@gmail.com>
2024-02-04 17:22:06 +0200

    Feat: logger is using single instance of console logger

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,21 +1,22 @@
 import { ConsoleLogger, Injectable } from '@nestjs/common';
 
+const logger = new ConsoleLogger();
+
 @Injectable()
 export class LoggerService {
   private _context = '';
-  private logger = new ConsoleLogger();
 
   debug(...message: any[]) {
     const msg = message.join(' ');
-    this.logger.debug(msg, this._context);
+    logger.debug(msg, this._context);
   }
 
   log(message: string) {
-    this.logger.log(message, this._context);
+    logger.log(message, this._context);
   }
 
   error(message: string, trace: string) {
-    this.logger.error(message, trace, this._context);
+    logger.error(message, trace, this._context);
   }
 
   static withContext(module: { name: string }) {
